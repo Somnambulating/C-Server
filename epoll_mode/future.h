3801cc68a46a9dcc72fcc4cf584402afbc848aab
Somnambulating <1366033875@qq.com>
2020-03-12 00:16:16 -0700

    update epoll_mode

---
+++ epoll_mode/future.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2020 Descosmos  
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source, with or without 
+ * modification, are permitted provided that following coditions:
+ *
+ *  1. Nither the name of copyright holders nor names of its 
+ *      contributors may be used to endorse or promote products 
+ *      derived from thise software without specific prior written 
+ *      permission.
+ *
+ *  2. Whatever the way contributors want to modify the code, 
+ *      it's doesn't care about the original copyright.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS.
+ *
+ * */
+
+#ifndef EPOLL_MODE_FUTURE_H_
+#define EPOLL_MODE_FUTURE_H_
+
+
+
+
+
+
+
+#endif
